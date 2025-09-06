95f74db310ed620d4faf36d0e6895a7fb754b033
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-06 18:15:17 +0100

    props and context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,13 @@
-import ProfilePage from "./components/ProfilePage";
-import UserContext from "./components/UserContext";
+import UserInfo from "./UserInfo";
+import UserProfile from "./UserProfile";
 
-function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
+function ProfilePage() {
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+    <>
+      <UserInfo />
+      <UserProfile />
+    </>
   );
 }
 
-export default App;
+export default ProfilePage;
---
+++ alx-react-app-props/src/UserProfile.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>
+        <strong>Name:</strong> {userData.name}
+      </p>
+      <p>
+        <strong>Email:</strong> {userData.email}
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
