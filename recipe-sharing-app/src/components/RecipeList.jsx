bb19887d47272e911ca4b6a6cac28bdaeb3968ae
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-09 12:02:20 +0100

    task 0 of recipe sharing app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from "./useRecipeStore";
+import useRecipeStore from "./RecipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
