9cdbff6b23f909dd8839524ccc41822d81341bb2
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-10 05:06:15 +0100

    zustand

---
+++ recipe-sharing-app/components/RecipeList.jsx
@@ -0,0 +1,25 @@
+import useRecipeStore from "./recipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.length === 0 ? (
+        <p>No recipes yet. Add one above!</p>
+      ) : (
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
