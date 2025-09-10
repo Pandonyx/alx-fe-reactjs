34ea6bd60cc744ec1d715fb0b72139b8f8dcd2ae
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-10 05:10:59 +0100

    zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import useRecipeStore from "./components/RecipeStore";
 import "./App.css";
 
 function App() {
