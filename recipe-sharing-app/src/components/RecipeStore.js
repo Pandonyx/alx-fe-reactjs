dcc020994e3eaefbce0cbad16d7710bff3bcac30
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-10 05:05:08 +0100

    zustand

--- recipe-sharing-app/src/components/RecipeStore.js
+++ recipe-sharing-app/src/components/RecipeStore.js
@@ -1,10 +1,33 @@
 import { create } from "zustand";
 
+/**
+ * @typedef {object} Recipe
+ * @property {number} id - The unique identifier for the recipe.
+ * @property {string} title - The title of the recipe.
+ * @property {string} description - The description of the recipe.
+ */
+
+/**
+ * @typedef {object} RecipeStoreState
+ * @property {Recipe[]} recipes - The list of recipes.
+ * @property {(newRecipe: Recipe) => void} addRecipe - Function to add a new recipe.
+ * @property {(recipes: Recipe[]) => void} setRecipes - Function to set all recipes.
+ * @property {(recipeId: number) => void} removeRecipe - Function to remove a recipe by its ID.
+ */
+
+/**
+ * Zustand store for managing recipes.
+ * @type {import("zustand").UseBoundStore<import("zustand").StoreApi<RecipeStoreState>>}
+ */
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
+  removeRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
 }));
 
 export default useRecipeStore;
