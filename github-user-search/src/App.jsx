c8b8a24cbc419cee833b3cd45b30a6e996182e88
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-19 10:07:11 +0100

    update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,48 +1,14 @@
-import { useState } from "react";
 import "./App.css";
-import SearchBar from "./components/Search";
-import UserProfileCard from "./components/UserProfileCard";
-import { fetchUserData } from "./services/githubService";
+import Search from "./components/Search";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSearch = async (username) => {
-    setIsLoading(true);
-    setError(null);
-    setUser(null);
-    try {
-      const userData = await fetchUserData(username);
-      setUser(userData);
-    } catch (err) {
-      setError(`User not found or an error occurred. Please try again.`);
-      console.error(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
     <div className='container'>
       <header>
         <h1>GitHub User Search</h1>
       </header>
       <main>
-        <SearchBar
-          onSearch={handleSearch}
-          isLoading={isLoading}
-        />
-
-        <div className='user-profile-container'>
-          {isLoading && <p>Loading...</p>}
-          {error && <p className='error'>{error}</p>}
-          {user && <UserProfileCard user={user} />}
-          {!isLoading && !error && !user && (
-            <p>Search for a user to see their profile.</p>
-          )}
-        </div>
+        <Search />
       </main>
     </div>
   );
