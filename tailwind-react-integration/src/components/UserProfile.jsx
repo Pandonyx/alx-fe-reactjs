56340a1397b94045f5b151dd373106c7fa0c3c69
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-09-28 22:19:58 +0100

    style: enhance UserProfile component with transition effects and hover states

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className='max-w-xs p-4 mx-auto my-20 bg-gray-100 rounded-lg shadow-lg user-profile sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm'>
+    <div className='max-w-xs p-4 mx-auto my-20 transition-shadow duration-300 ease-in-out bg-gray-100 rounded-lg shadow-lg user-profile sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl'>
       <img
         src='https://via.placeholder.com/150'
         alt='User'
-        className='w-24 h-24 mx-auto rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36'
+        className='w-24 h-24 mx-auto transition-transform duration-300 ease-in-out rounded-full cursor-pointer sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110'
       />
-      <h1 className='my-4 text-lg text-center text-blue-800 sm:text-lg md:text-xl'>
+      <h1 className='my-4 text-lg text-center text-blue-800 transition-colors duration-300 ease-in-out cursor-pointer sm:text-lg md:text-xl hover:text-blue-500'>
         John Doe
       </h1>
       <p className='text-sm text-center text-gray-600 sm:text-sm md:text-base'>
